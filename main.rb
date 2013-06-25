d1b4ba390d511db51970fcd8bf985b356324a5b5
Tim <tim.blonski@gmail.com>
2013-06-25 16:38:48 -0400

    links update

--- main.rb
+++ main.rb
@@ -5,3 +5,22 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+get '/manual_links' do
+  @links = ['http://reddit.com', 'http://techcrunch.com', 'http://gizmodo.com']
+
+  erb :links
+end
+
+get '/links' do
+  link_file = File.new('links.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    parsed_link = line.split(',')
+    @links << parsed_link
+end
+link_file.close
+
+erb :links
+end
+
